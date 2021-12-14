13f86389296c5234c54d30a05bea7985c86b38ec
Jasleen Kaur <jasleenkaur90202@gmail.com>
2021-12-15 00:16:36 +0530

    added find and findIndex

--- JavaScript/arrays4_some.js
+++ JavaScript/arrays4_some.js
@@ -1,16 +1,7 @@
+// some takes each value 1 by 1 nd returns a true if alteast 1 cb returns true
+// return false only if all cb return false
 
 //custom some function
-let arr = [
-    {name:"A", age: "14", gender: "M"},
-    {name:"B", age: "34", gender: "M"},
-    {name:"C", age: "40", gender: "F"},
-    {name:"D", age: "44", gender: "F"},
-    {name:"E", age: "44", gender: "M"},
-    {name:"F", age: "38", gender: "F"},
-    {name:"G", age: "36", gender: "M"},
-    {name:"H", age: "27", gender: "F"}
-]
-
 Array.prototype.mySome = function(callback){
     let res = false;
     for(let i =0; i<this.length; i++)
@@ -26,6 +17,18 @@ Array.prototype.mySome = function(callback){
     return res;
 }
 
+let arr = [
+    {name:"A", age: "14", gender: "M"},
+    {name:"B", age: "34", gender: "M"},
+    {name:"C", age: "40", gender: "F"},
+    {name:"D", age: "44", gender: "F"},
+    {name:"E", age: "44", gender: "M"},
+    {name:"F", age: "38", gender: "F"},
+    {name:"G", age: "36", gender: "M"},
+    {name:"H", age: "27", gender: "F"}
+]
+
+//Is there a valid candidate (F and between 20 and 30)
 let rres = arr.filter(function(v, i, oarr){
     return v.gender == 'F';
 }).mySome(function(v, i, oarr){
--- JavaScript/arrays5_Every.js
+++ JavaScript/arrays5_Every.js
@@ -1,15 +1,6 @@
+// if every callback return true then "Every" return true otherwise false
 
 //custom every function
-let arr = [
-    {name:"A", age: "14", gender: "M"},
-    {name:"B", age: "34", gender: "M"},
-    {name:"C", age: "20", gender: "F"},
-    {name:"D", age: "24", gender: "F"},
-    {name:"E", age: "44", gender: "M"},
-    {name:"F", age: "28", gender: "F"},
-    {name:"G", age: "36", gender: "M"},
-    {name:"H", age: "37", gender: "F"}
-]
 
 Array.prototype.myEvery = function(callback){
     let res = true;
@@ -23,6 +14,20 @@ Array.prototype.myEvery = function(callback){
     }
     return res;
 }
+
+let arr = [
+    {name:"A", age: "14", gender: "M"},
+    {name:"B", age: "34", gender: "M"},
+    {name:"C", age: "20", gender: "F"},
+    {name:"D", age: "24", gender: "F"},
+    {name:"E", age: "44", gender: "M"},
+    {name:"F", age: "28", gender: "F"},
+    {name:"G", age: "36", gender: "M"},
+    {name:"H", age: "37", gender: "F"}
+]
+
+
+// Are all Female Candidates Valid(F and between 20 and 30)
 
 let rres = arr.filter(function(v, i, oarr){
     return v.gender == 'F';
